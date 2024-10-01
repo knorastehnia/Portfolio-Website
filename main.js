c68f24c00f716bbfd7d70d69907ac8c560ad3cda
Karan-Norastehnia <98152815+Karan-Norastehnia@users.noreply.github.com>
2024-10-01 16:51:55 -0400

    Improved visuals and transitions

--- main.js
+++ main.js
@@ -1,17 +1,36 @@
 const overview = document.querySelector("#overview");
 const scroll_button = document.querySelector("#scroll-down");
-const icons = document.querySelectorAll(".icon");
+const concealer = document.querySelector(".concealer");
+const cards = document.querySelectorAll(".card");
+
+const default_scroll_height = "25%";
 
 window.onscroll = function (e) {
-    if (window.scrollY > 150) {
+    if (window.scrollY > 250) {
         overview.classList.add("overview-left");
         overview.classList.remove("overview-center");
 
-        scroll_button.style.display = "none";
+        scroll_button.classList.add("hidden");
+
+        concealer.classList.add("hidden");
+
+        overview.style.top = window.scrollY / document.body.scrollHeight * 50 + 25 + '%';
     } else {
         overview.classList.add("overview-center");
         overview.classList.remove("overview-left");
-        
-        scroll_button.style.display = "inline";
+
+        scroll_button.classList.remove("hidden");
+
+        concealer.classList.remove("hidden");
+
+        overview.style.top = "50%";
     }
+
+    cards.forEach(card => {
+        if (Math.abs(card.getBoundingClientRect().y) < 600) {
+            card.classList.add("card-visible");
+        } else {
+            card.classList.remove("card-visible");
+        }
+    });
 }
